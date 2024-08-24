5de40216e2460ad806c02f0aaa945009cdbe1ec3
yosseftame <151669320+yosseftame@users.noreply.github.com>
2024-08-24 14:19:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// Data types
+/*
+   string
+   number
+   object-> list //array
+   {key:value}-> object
+   boolean -> true false
+   undefine
+   null
+*/
+
+/*
+var:
+  redefine
+  access before define => NO
+  added to window
+let:
+  no redefine
+  access before define => NO
+  not added to window
+const:
+  no redefine
+  access before define => NO
+
+*/
+
+
